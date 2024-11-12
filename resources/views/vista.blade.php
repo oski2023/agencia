1e7476e4373a2656f8fb76aac4264547b966e7b7
Oscar Marcelo <marceloriquelme78@gmail.com>
2024-11-12 16:58:36 -0300

    Dentro de la carpeta resources en el archivi web.php se generan otras peticiones con get como /mensaje.html y /vista ademas se crea en la carpeta layouts en views donde se crean los archivos header y footer

---
+++ resources/views/vista.blade.php
@@ -0,0 +1,29 @@
+@include('layouts.header')
+
+
+<main class="container">
+    <h1>Mi primer Vista en Laravel</h1>
+
+    <!--  handlebars o mustache -->
+    Nombre: {{ $nombre }}
+    <hr>
+    Edad: {{ $numero }}
+    <hr>
+    <!-- directivas blade -->
+    @if ($numero > 18)
+    Eres mayor de edad
+    <hr>
+    @else
+    Eres menor de edad
+    @endif
+
+    <ul>
+        @foreach ($datos as $clave => $elemento)
+        <li>{{ $elemento }}</li>
+    </ul>
+    @endforeach
+
+</main>
+
+@include('layouts.footer')
+
--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,31 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+//Route::view('peticion','nombreVista');
+//Route::view('/vista', 'vista');
 
+//Route::get('peticion', 'accion)
+Route::get('/mensaje.html', function () 
 
+        {
+            return 'Hola mundo desde LARAVEL chameeeego';
+        }
+
+);
+
+Route::get('/vista', function()
+{
+    $nombre = 'oski';
+    $numero = 46;
+    $datos = [
+        'curso' => 'Desarrollo web con LARAVEL',
+        'codigo' => 71994,
+        'inicio' => '04/11/2024',
+        'fin' => '16/12/2024'
+    ];
+    return view('vista', [
+        'nombre' => $nombre,
+        'numero' => $numero,
+        'datos' => $datos
+    ]);
+});
